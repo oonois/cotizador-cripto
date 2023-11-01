5bc01d63f611c0d2ed46e51547aa8c903c057ea1
oonois <oonois@gmail.com>
2023-11-01 12:09:35 +0100

    cotizador cripto

---
+++ src/components/Error.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled'
+
+const Texto = styled.div`
+  background-color: #B7322C;
+  color:#fff;
+  padding: 15px;
+  font-size:22px;
+  text-transform: uppercase;
+  font-family:'Lato', sans-serif;
+  text-align: center;
+  font-weight: 700;
+`
+
+const Error = ({children}) => {
+  return (
+    <Texto>
+      {children}
+    </Texto>
+  )
+}
+
+export default Error
---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+  {id:'USD', nombre:'Dolar de Estads Unidos'},
+  {id:'EUR', nombre:'Euro'},
+  {id:'MXN', nombre:'Peso Mejicano'},
+  {id:'GBP', nombre:'Libra Esterlina'}
+]
+
+export {
+  monedas
+}
